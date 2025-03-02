b3cb7cc0d90f8f6ac10e2d05ae0f23e05b743903
Agénor Debriat <agenor@ylly.fr>
2025-03-02 17:06:36 +0100

    🎉 Initial commit

---
+++ app/app.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const items = [
+  {
+    label: "Avantages",
+    to: "#avantages",
+    icon: "heroicons:bolt-20-solid",
+  },
+  {
+    label: "Témoignages",
+    to: "#temoignages",
+    icon: "heroicons:chat-bubble-left-right-20-solid",
+  },
+  {
+    label: "Formules",
+    to: "#formules",
+    icon: "heroicons:information-circle-20-solid",
+  },
+  {
+    label: "Me contacter",
+    to: "#contact",
+    icon: "heroicons:envelope-20-solid",
+  },
+];
+</script>
+
+<template>
+  <NuxtRouteAnnouncer />
+  <UApp>
+    <UContainer>
+      <UHeader title="Congés Tabac Presse">
+        <UNavigationMenu :items />
+        <template #body>
+          <UNavigationMenu :items />
+        </template>
+        <template #right>
+          <UColorModeButton />
+          <UButton to="#devenir-partenaire" label="Devenir partenaire" />
+        </template>
+      </UHeader>
+      <NuxtPage />
+      <UFooter />
+    </UContainer>
+  </UApp>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+  future: { compatibilityVersion: 4 },
+  modules: ["@nuxt/ui-pro", "@nuxt/content"],
+});
